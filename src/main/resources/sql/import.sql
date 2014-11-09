f5695174b299e1843dd1f6d4d7ff0c94907bb17e
Raphaël <billard.raphael@gmail.com>
2014-11-09 11:41:31 +0100

    import.sql

--- src/main/resources/sql/import.sql
+++ src/main/resources/sql/import.sql
@@ -4,4 +4,4 @@ INSERT INTO Movie (id, title) VALUES (1, 'Pirates of the Caribbean');
 INSERT INTO Movie (id, title) VALUES (2, 'Edward Scissorhands');
 INSERT INTO Role (id, ACTOR_ID, MOVIE_ID, firstName, lastName) VALUES (1, 1, 1, 'Jack', 'SPARROW');
 INSERT INTO Role (id, ACTOR_ID, MOVIE_ID, firstName, lastName) VALUES (2, 2, 1, 'Will', 'TURNER');
-INSERT INTO Role (id, ACTOR_ID, MOVIE_ID, firstName, lastName) VALUES (3, 1, 2, 'Edward', 'Scissorhands');
+INSERT INTO Role (id, ACTOR_ID, MOVIE_ID, firstName, lastName) VALUES (3, 1, 2, 'Edward', 'SCISSORHANDS');
